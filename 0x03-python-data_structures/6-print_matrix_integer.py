dd8da10849c25d70491de36a3b2a4553e938bdc6
Cassyeh <cassandraijezie@gmail.com>
2023-03-13 12:49:11 -0700

    Correction of Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    length = len(matrix)
-    for i in range(0, length):
-        lengtha = len(matrix[i])
-        a = matrix[i]
-        for j in range(0, lengtha):
-            if j != lengtha - 1:
-                print("{}".format(a[j]), end=" ")
+    for row in matrix:
+        for val in row:
+            if val != row[-1]:
+                print(val, end=" ")
             else:
-                print("{}".format(a[j]), end = "")
+                print(val, end="")
         print()
