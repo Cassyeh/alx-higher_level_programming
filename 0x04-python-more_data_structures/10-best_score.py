11f5a12e68bc4220a572ca579e8801323118ec8d
Cassyeh <cassandraijezie@gmail.com>
2023-03-15 21:33:33 -0700

    Task 12

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,8 +4,6 @@ def best_score(a_dictionary):
         return (None)
     a = list(a_dictionary)
     length = len(a)
-    if length == 0:
-        return (None)
     big = a_dictionary[a[0]]
     for i in range(0, length):
         if a_dictionary[a[i]] >= big:
