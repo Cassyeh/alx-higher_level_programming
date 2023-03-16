523a5118c6ffce4a3e08e74b7af031be99ed5d9a
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-03-16 22:34:24 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == None:
+    if roman_string is None:
         return (0)
     if type(roman_string) != str:
         return (0)
