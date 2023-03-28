950c78adcd9944e38bf595bfb7fef98e0453adf6
Cassyeh <cassandraijezie@gmail.com>
2023-03-28 08:23:49 -0700

    Correction of Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,8 +8,7 @@ def safe_print_list(my_list=[], x=0):
         try:
            print("{:d}".format(my_list[i]), end="")
            b = b + 1
-           if i == x - 1 or i == a - 1:
-               print()
         except IndexError:
             break
+    print()
     return (b)
