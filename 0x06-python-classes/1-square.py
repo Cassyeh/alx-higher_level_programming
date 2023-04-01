34a688978b418e8d4c22fe5f8110fdcca44a67b9
Cassyeh <cassandraijezie@gmail.com>
2023-04-01 02:07:56 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+Defines class Square
+"""
+
+
+class Square:
+    """
+    An empty class that defines a square
+    """
+    def __init__(self, size):
+        self.__size = size
