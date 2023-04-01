a900e5fb260bbc213711e51f1d98fb8f0eef986e
Cassyeh <cassandraijezie@gmail.com>
2023-04-01 08:21:55 -0700

    Correction of Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -22,6 +22,12 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
+        if type(position) != tuple:
+            raise TypeError("position must be a tuple of 2 positive numbers")
+        if position[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive numbers")
+        if position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive numbers")
 
     @property
     def size(self):
