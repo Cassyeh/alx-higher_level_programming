5aa5188ff7d22652d617823632e91a7efca9a946
Cassyeh <cassandraijezie@gmail.com>
2023-04-01 06:05:32 -0700

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,17 +9,27 @@ class Square:
     """
     A class that defines a square
     Private instance attribute: size
+    property def size(self): to retrieve it
+    property setter def size(self, value): to set it
     Instantiation with size and error messages
     Public instance method: def area(self): that
     returns the current square area
     """
     def __init__(self, size=0):
         self.__size = size
-        if type(size) != int:
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-        area = self.__size ** 2
+        area = self.size ** 2
         return area
