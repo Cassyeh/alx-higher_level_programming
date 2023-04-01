6ae421ac2423f4da7b40c6af5085494f41e70bd3
Cassyeh <cassandraijezie@gmail.com>
2023-04-01 05:08:13 -0700

    Task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,7 +7,9 @@ Defines class Square
 
 class Square:
     """
-    An empty class that defines a square
+    A class that defines a square
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
     """
     def __init__(self, size):
         self.__size = size
