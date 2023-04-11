bc653452e235539f2be0af73c5206a8863754506
Cassyeh <cassandraijezie@gmail.com>
2023-04-11 09:00:41 -0700

    Pycode style for Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,9 +3,12 @@
 Module
 Please refer to the documentation in the README.md
 """
+
+
 def lookup(obj):
     """
-    function that returns the list of available attributes and methods of an object
+    function that returns the list of available
+    attributes and methods of an object
     """
     res = dir(obj)
     return(res)
