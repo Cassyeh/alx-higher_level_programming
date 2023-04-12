5b638df07ca4a22416a76ad3366413cfc730545e
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-04-12 06:04:50 +0100

    Create 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+Defines class Square
+"""
+
+
+def is_kind_of_class(obj, a_class):
+  """
+  An empty class that defines a square
+  """
+  result = isinstance(obj, a_class)
+    return result
