400f377e29afa7bdffe6f1ed38b973c84f827dc1
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-06-14 15:39:09 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const Square = require('./5-square');
+class Square extends Square {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print ()
+    } else {
+      let i;
+      let j;
+      let msg = '';
+      if (this.width > 0 && this.height > 0) {
+        for (i = 0; i < this.height; i++) {
+          for (j = 0; j < this.width; j++) {
+            msg = msg + c;
+          }
+          if (i !== this.height - 1) {
+            msg = msg + '\n';
+          }
+        }
+        console.log(msg)
+      }
+    }
+  }
+}
