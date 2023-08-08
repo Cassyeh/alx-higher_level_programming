ad182bc403bc1ad12bb9cce65ddf59fa05f7d433
Cassyeh <cassandraijezie@gmail.com>
2023-08-08 04:36:22 -0700

    Trying a different code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,20 +8,39 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	int i = 0, j;
+	listint_t *temp, *curr;
 
-	if (!list)
-		return (0);
-
-	while (slow && fast && fast->next)
+	if (list == NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		return (0);
+	}
+	curr = list;
+	temp = list;
+	if (list->next == NULL)
+	{
+		return (0);
+	}
+	list = list->next;
+	while (list != NULL)
+	{
+		j = 0;
+		while (curr != NULL)
 		{
-			return (1);
+			if (curr == list)
+			{
+				return (1);
+			}
+			if (j == i)
+			{
+				break;
+			}
+			j++;
+			curr = curr->next;
 		}
+		curr = temp;
+		list = list->next;
+		i++;
 	}
 	return (0);
 }
