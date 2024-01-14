9f268f1ed9bb3ba3adec967eb0d8fc74362371c7
Cassyeh <cassandraijezie@gmail.com>
2024-01-13 21:06:30 -0800

    Camelcase

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,20 +6,20 @@ request(process.argv[2], function (error, _res, body) {
   if (error) {
     console.log(error);
   } else {
-    const completed_tasks = {};
+    const completedTasks = {};
     body = JSON.parse(body);
 
     for (let i = 0; i < body.length; ++i) {
       const userId = body[i].userId;
       const completed = body[i].completed;
 
-      if (completed && !completed_tasks[userId]) {
-        completed_tasks[userId] = 0;
+      if (completed && !completedTasks[userId]) {
+        completedTasks[userId] = 0;
       }
 
-      if (completed) ++completed_tasks[userId];
+      if (completed) ++completedTasks[userId];
     }
 
-    console.log(completed_tasks);
+    console.log(completedTasks);
   }
 });
